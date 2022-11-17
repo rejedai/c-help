e183102fd32e65c2129b4cbf58946a0560f5abf6
rejedai <92713267+rejedai@users.noreply.github.com>
2022-11-17 21:51:41 +0300

    Update H01FOR1.c

--- H01FOR1.c
+++ H01FOR1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int result = 1;
+    int result = 1; // результат тут будет конечный результат суммы
     int n;
     
     scanf("%d", &n);
     for(int a=2; a<=n; a++){
-        result+=a;
+        result+=a; // увеличиваем результат на a
     }
     printf("%d", result);
 }
