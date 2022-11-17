919994044587fad46a267a2f546c3cb1cdc913ce
rejedai <92713267+rejedai@users.noreply.github.com>
2022-11-17 22:54:41 +0300

    Update H03FOR3.c

--- H03FOR3.c
+++ H03FOR3.c
@@ -7,11 +7,11 @@ int main() {
     scanf("%d", &n);
     
     int result=1;
-    int exp_a = a;
+    int exp_a = a; // exp это экспоненета т.е. степень в переводе с англ.
     
     for(int i=1; i<=n; i++){
         result += exp_a;
-        exp_a *= a;
+        exp_a *= a; // каждый проход цикла увеличиваем степень a
     }
     
     printf("%d ", result);
