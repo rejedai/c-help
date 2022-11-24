e3092066f340852d8a3a9fd0b21ebc6865a9380c
rejedai <92713267+rejedai@users.noreply.github.com>
2022-11-24 15:55:24 +0300

    Update H05ARR1.c

--- H05ARR1.c
+++ H05ARR1.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-void max_item(int *m_pointer, int m_count) {
+int max_item(int *m_pointer, int m_count) {
     int max = -32767;
     for(int a=0; a<m_count; a++) {
         if(max < m_pointer[a]) {
             max = m_pointer[a];
         }
     }
-    printf("%d", max);
+    return max;
 }
 
 int main() {
-    long int m[] = {3, 4, 1, 7};
+    int m[] = {3, 4, 1, 7};
     
-    //вызов функции
-    max_item(&m, sizeof(m)/sizeof(m[0]));
+    printf("%d", max_item(&m, sizeof(m)/sizeof(m[0])));
     return 0;
 }
